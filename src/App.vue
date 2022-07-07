c660ee273e526a13dded53286a1fcee1e48ab9ce
Mahmut Erkul <mahmut.erkul@kafein.com.tr>
2022-07-07 22:47:30 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+    <h2 :value="appname"></h2>
+    <ul>
+        <li v-for="note in dummyNotes" :key="note.id">
+            <Note :note="note"></Note>
+        </li>
+    </ul>
+</template>
+<script>
+import Note from "./components/Note.vue";
+export default {
+    data() {
+        return {
+            appname: "notes",
+            dummyNotes: [
+                {
+                    id: 1,
+                    name: "Credit Card Payment",
+                    description: "Bonus card must be payed by 7th of July"
+                },
+                {
+                    id: 2,
+                    name: "Credit Card Payment",
+                    description: "World card must be payed by 7th of July"
+                },
+                {
+                    id: 3,
+                    name: "Credit Payment",
+                    description: "Credit payment is due on 5fth of august"
+                }
+            ]
+        };
+    },
+    components: { Note }
+}
+</script>
---
+++ src/components/Note.vue
@@ -0,0 +1,23 @@
+<template>
+    <section>
+        <div>
+            <label :for="note.id">Name</label>
+            <input type="text" disabled :id="note.id" :value="note.name" />
+        </div>
+        <div>
+            <label :for="note.id">Detail</label>
+            <input type="text" disabled :id="note.id" :value="note.description" />
+        </div>
+    </section>
+</template>
+
+<script>
+export default {
+    props: {
+        note: {
+            type: Object,
+            required: true
+        }
+    },
+}
+</script>
